952e95b73ddd880b83a116dd37e47c39870ea64d
Rick van den Bosch <rickvdbosch@outlook.com>
2021-04-02 12:15:06 +0200

    Add CSharp as a language Calling the CSharp generator

--- tools/RandomCodeGenerator.js
+++ tools/RandomCodeGenerator.js
@@ -1,9 +1,11 @@
+import CSharp from "./utils/csharp";
 import Css from "./utils/css";
 import JavaScript from "./utils/javascript";
 
 export const Languages = {
   js: "JavaScript",
   css: "CSS",
+  csharp: "C#"
 };
 
 export function getRandomInt(min, max) {
@@ -29,20 +31,35 @@ export function generateRandomCode(language, lines) {
 
       lastLine = "\n\r}";
       return firstLine + fillerLines.join("\n\r") + lastLine;
-    case "js":
-      firstLine = `function ${JavaScript.getRandomFunctionName()}() {\n\r`;
+    case "csharp":
+      firstLine = (`${CSharp.getRandomAccessModifier()} ${CSharp.getRandomMethodKeyword()} void ${CSharp.getRandomMethodName()}()\n\r`).replace('  ', ' ');
+      if (firstLine[0] === ' ') firstLine = firstLine.slice(1);
 
-      fillerLineQty = parseInt(lines, 10) - 2;
+      fillerLineQty = parseInt(lines, 10) - 3;
       fillerLines = [];
 
+      fillerLines.push('{');
       for (let i = 1; i <= fillerLineQty; i++) {
-        fillerLines.push(`    ${JavaScript.getRandomFillerLine()}`);
+        fillerLines.push(`    ${CSharp.getRandomFillerLine()}`);
       }
 
       lastLine = "\n\r}";
 
       return firstLine + fillerLines.join("\n\r") + lastLine;
-    default:
+    case "js":
+        firstLine = `function ${JavaScript.getRandomMethodName()}() {\n\r`;
+  
+        fillerLineQty = parseInt(lines, 10) - 2;
+        fillerLines = [];
+  
+        for (let i = 1; i <= fillerLineQty; i++) {
+          fillerLines.push(`    ${JavaScript.getRandomFillerLine()}`);
+        }
+  
+        lastLine = "\n\r}";
+  
+        return firstLine + fillerLines.join("\n\r") + lastLine;
+      default:
       return "lol";
   }
 }
