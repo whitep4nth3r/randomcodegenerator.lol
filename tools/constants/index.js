46d1cec2d9bbb90ca3474b51393965a3b1e3df74
rawrsatbeards <code@rawrsatbeards.com>
2021-04-07 01:28:39 +0200

    :bug: Fix constant index import error

--- tools/constants/index.js
+++ tools/constants/index.js
@@ -1,4 +1,4 @@
-export { default as Contributors } from "./Contributors";
+export { default as Contributors } from "./contributors";
 export { default as logs } from "./logs";
 export { default as nouns } from "./nouns";
 export { default as singleCharacters } from "./singleCharacters";
