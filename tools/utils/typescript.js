47f7bfb91fc60f20638caa4cf33677444ef2856f
@lukeocodes <luke@lukeoliff.com>
2021-04-09 10:33:39 +0100

    feat: add random properties to TS function

--- tools/utils/typescript.js
+++ tools/utils/typescript.js
@@ -1,8 +1,24 @@
 import JavaScript from "./javascript";
 
-import { getRandomEntry, getRandomNoun, getRandomLogLine } from "./helpers";
+import {
+  getRandomInt,
+  getRandomEntry,
+  getRandomNoun,
+  getRandomLogLine,
+} from "./helpers";
+
+import {
+  singleCharacters,
+} from "../constants";
 
 export default class TypeScript extends JavaScript {
+  static getRandomProperties() {
+    const start = getRandomInt(0, singleCharacters.length-1)
+    const end = getRandomInt(start + 1, singleCharacters.length)
+
+    return singleCharacters.slice(start, end)
+  }
+
   static getRandomVariableDeclaration() {
     const keyWords = ["let", "const", "var"];
 
